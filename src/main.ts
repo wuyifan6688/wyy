0cc0b68fcef6051f27bb3dc24ae3287dc8bd9c3d
itcast <hello@itcast.cn>
2023-09-07 13:41:26 +0800

    一致

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import 'normalize.css';
 import { createPinia } from 'pinia';
 import VueVirtualScroller from 'vue-virtual-scroller';
 import { createApp } from 'vue';
-import App from './App.vue';
 import './assets/css/common.css';
 import './assets/css/transition.css';
 import './index.css';
+import App from './App.vue';
 import router from './router';
 const app = createApp(App);
 
