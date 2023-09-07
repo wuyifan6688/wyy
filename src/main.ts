0492ab2d86f97660ca6faaf2c368eb260c25d1b2
itcast <hello@itcast.cn>
2023-09-07 13:50:36 +0800

    调整

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import 'normalize.css';
+import 'normalize.css';               // 第三方库样式
+import './assets/css/common.css';     // 通用样式
+import './assets/css/transition.css'; // 过渡或动画样式
+import VueVirtualScroller from 'vue-virtual-scroller'; // 组件库样式（如果有）
+import './index.css';                 // 其他特定样式
+
 import { createPinia } from 'pinia';
-import VueVirtualScroller from 'vue-virtual-scroller';
 import { createApp } from 'vue';
-import './assets/css/common.css';
-import './assets/css/transition.css';
-import './index.css';
 import App from './App.vue';
 import router from './router';
 const app = createApp(App);
